63dce9b20e6bdb46d41daa3f5f60f332a79ff341
Angelo <angelo.poerio@gmail.com>
2018-02-11 23:21:43 +0100

    classical overflow

--- basic/basic.c
+++ basic/basic.c
@@ -2,17 +2,17 @@
 	A simple uber exploitable program.
 	How to test:
 	1) Disable ALSR: echo "0" > /proc/sys/kernel/randomize_va_space 
-	2) Compile with executable stack and stack smashing off: cc old_school.c -o old_school -zexecstack -fno-stack-protector -g
+	2) Compile with executable stack and stack smashing off: cc basic.c -o basic -zexecstack -fno-stack-protector -g
 */
 
 #include <string.h>
 
-void vuln(char *bf) {
+void vuln() {
 	char buf[80];
 	read(0,buf,400);
 }
 
 int main(int argc, char **argv) {
     char buf[20];
-    vuln(argv[1]);
+    vuln();
 }
